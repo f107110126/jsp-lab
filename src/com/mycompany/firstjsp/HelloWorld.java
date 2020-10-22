e1b9d507b6310428213d9a1803c38e6c58f345e6
Andy.Chen <f107110126@gmail.com>
2020-10-23 01:36:01 +0800

    first servlet

---
+++ src/com/mycompany/firstjsp/HelloWorld.java
@@ -0,0 +1,27 @@
+package com.mycompany.firstjsp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "HelloWorld", urlPatterns = { "/helloworld" })
+
+public class HelloWorld extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Hello! World!</title>");
+        out.println("<body>");
+        out.println("<h1>Hello! World!</h1>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
